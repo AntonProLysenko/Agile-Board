4c61678f47cf27215b5c8a3fcdedf9f316a926ff
boldPro <probold777@gmail.com>
2023-02-04 11:47:10 -0500

    accesed params on show page

--- client/src/screens/Show.js
+++ client/src/screens/Show.js
@@ -1,5 +1,38 @@
+import{useState, useEffect} from "react"
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
 function Show() {
-    return <div>Show Page</div>;
-  }
+  const idpar = useParams();
+
+  console.log(idpar.id);
   
-  export default Show;
+  const [task, setTask] = useState({});
+  useEffect(() => {
+
+    async function getTask(id) {
+      try {
+        const {data} = await axios.get(`http://localhost:3001/tasks/${id}`);
+
+        console.log(data);
+        
+        setTask(data);
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getTask(idpar.id);
+  }, []);
+
+  return(
+
+    <div>
+      <h1>{task.entry}</h1>
+      <p>{task.body}</p>
+    </div>
+  ) 
+}
+
+export default Show;
