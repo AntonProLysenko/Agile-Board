16f7ad3e74f7a2948e333b1f150baa4fc433541e
boldPro <probold777@gmail.com>
2023-02-05 13:30:52 -0500

    added layout for new task entry

--- client/src/components/AddTaskForm.js
+++ client/src/components/AddTaskForm.js
@@ -5,29 +5,40 @@ export default function AddTaskForm({open, entry, body, statusRef, handleSubmit,
     if(!open) return null
 
   return (
-      <div className='overlay'>
-        <div className="formContainer">
-          <button onClick={onClose}>{closeIcon}</button>
-          <form className="form" onSubmit={handleSubmit}>
-            <label>
-              Entry: <input type="text" ref={entry} />
-            </label>
-            <label>
-              Body: <textarea type="text" ref={body} />
-            </label>
-            <label>
-              Status:
-              <select ref={statusRef}>
-                <option value="to-do">To-Do</option>
-                <option value="pending">Pending</option>
-                <option value="completed">Completed</option>
-              </select>
-            </label>
-            <button type="submit" className="submit">
-              {plusIcon} &nbsp; Add
-            </button>
-          </form>
-        </div>
+    <div className="overlay">
+      <div className="formContainer">
+        <button className="close" onClick={onClose}>
+          {closeIcon}
+        </button>
+        <form className="form" onSubmit={handleSubmit}>
+
+     
+            <input
+              type="text"
+              ref={entry}
+              placeholder="Enter title"
+            />
+
+
+          <textarea
+            type="text"
+            ref={body}
+            placeholder="Enter detailed information or hints here"
+          />
+
+          <label>
+            Status:
+            <select ref={statusRef}>
+              <option value="to-do">To-Do</option>
+              <option value="pending">Pending</option>
+              <option value="completed">Completed</option>
+            </select>
+          </label>
+          <button type="submit" className="submit">
+            {plusIcon} &nbsp; Add
+          </button>
+        </form>
       </div>
+    </div>
   );
 }
