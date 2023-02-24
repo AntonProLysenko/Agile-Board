30d26a24ff1f623bca0907c67e9915f63f308123
boldPro <probold777@gmail.com>
2023-02-24 12:40:21 -0500

    fixed user basic route

--- client/src/utilities/users-api.js
+++ client/src/utilities/users-api.js
@@ -1,35 +1,34 @@
+const BASIC_URL = process.env.REACT_APP_BASIC_URL;
 
 export async function signUp(userData) {
-//   return sendRequest("/api/users", "POST", userData);
+  //   return sendRequest("/api/users", "POST", userData);
 
-
-const res = await fetch("http://localhost:3001/api/users",{
-        method: "POST",
-        headers : { 'Content-Type': 'application/json' },
-        body: JSON.stringify(userData)
-    });
-    if (res.ok){//if success
-        return res.json()
-    }else{
-        throw new Error("Invalid Sign Up")
-    }
+  const res = await fetch(`${BASIC_URL}/api/users`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(userData),
+  });
+  if (res.ok) {
+    //if success
+    return res.json();
+  } else {
+    throw new Error("Invalid Sign Up");
+  }
 }
 
-
-export async function logIn(credentials){
-    const res = await fetch("http://localhost:3001/api/users/login",{
-          method: "POST",
-        headers : { 'Content-Type': 'application/json' },
-        body: JSON.stringify(credentials)
-
-    })
-     if (res.ok){//if success
-        return res.json()
-    }else{
-        throw new Error("Invalid Log In")
-    }
+export async function logIn(credentials) {
+  const res = await fetch(`${BASIC_URL}/api/users/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(credentials),
+  });
+  if (res.ok) {
+    //if success
+    return res.json();
+  } else {
+    throw new Error("Invalid Log In");
+  }
 }
-
 
 // async function sendRequest(url, method = 'GET', payload = null) {
 //   // Fetch accepts an options object as the 2nd argument
