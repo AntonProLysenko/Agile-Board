b2d59208bb7e44e50aa38dac3a6da027b00a23d2
boldPro <probold777@gmail.com>
2023-05-08 15:31:37 -0400

    started styling edit form styles--date=Fri May12 05:23:21 -0400

--- client/src/components/AddTaskForm.js
+++ client/src/components/AddTaskForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import moment from "moment";
 
 
 
@@ -9,19 +10,35 @@ export default function AddTaskForm({open, entry, body, handleSubmit, handleUpda
 
 
      if(task._id){  
+          let lastUpdate = moment(task.updatedAt).fromNow();
         return(
           <div className="editOverlay" onClick={onClose}>
             <div className="modalContainer">
+              <div className="showHeader">
               <button className="close editClose" onClick={onClose}>
                  x
               </button>
-              <form className="form" onSubmit={(evt)=>{handleUpdate(evt, task._id)}}>
-                <input type="text" ref={entry} placeholder="Enter title"  defaultValue = {task.entry}/>
-                <textarea type="text" ref={body}      placeholder="Enter detailed information or hints here" defaultValue = {task.body}/>
-                <button  type="submit" className="button">
-                  Update
-                </button>
+                  <form className="form" onSubmit={(evt)=>{handleUpdate(evt, task._id)}}>
+                    <input className="listTitle" type="text" ref={entry} placeholder="Enter title"  defaultValue = {task.entry}/>
+                  </form>
+                  <div className="secondaryInfo">
+                    <p className="taskStatus">
+                      In {task.status.charAt(0).toUpperCase() + task.status.slice(1).toLowerCase()}{" "}list
+                    </p>
+
+                    <p className="date">Updated: {lastUpdate} </p>
+                  </div>
+               </div>
+               <div className="showInfo">
+                  <h3>Instructions:</h3>
+                  <form className="form" onSubmit={(evt)=>{handleUpdate(evt, task._id)}}>
+                    <textarea className="emptyInstructions" type="text" ref={body}      placeholder="Enter detailed information or hints here" defaultValue = {task.body}/>
+                  <button  type="submit" className="button">
+                    Update
+                  </button>
               </form>
+              </div>
+              <div className="pseudo"></div>
             </div>
           </div>
         )
