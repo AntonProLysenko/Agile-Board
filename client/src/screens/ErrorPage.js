eb21a1afc969af9c008ca821a6cad4b06d1338b2
boldPro <probold777@gmail.com>
2023-06-03 21:33:49 -0400

    edited vhs

--- client/src/screens/ErrorPage.js
+++ client/src/screens/ErrorPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function ErrorPage({errorMessage, errorCode} ) {
   console.log(errorMessage);
@@ -6,11 +7,13 @@ export default function ErrorPage({errorMessage, errorCode} ) {
 
   return (
     <>
-      <div>ErrorPage</div>;
-      <p>
-        {errorCode}
-        {errorMessage}
-      </p>
+      <div className='error-page'>
+      <h1>Error</h1>
+      <h1> {errorCode} </h1> 
+      <h2>{errorMessage}</h2>
+      <Link to ="/"><h3>Go back to main page</h3></Link>
+
+      </div>
     </>
   );
 }
